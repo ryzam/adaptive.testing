892c5dce44c1397a7bd1f70734618a91f9d95e4f
irwan.azam@cloud-connect.asia <irwan.azam@cloud-connect.asia>
2022-04-21 07:31:27 +0800

    Add testing project

---
+++ adaptive.puffy.testing/EndpointsTest/EndpointApiTest.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace adaptive.puffy.testing.EndpointsTest
+{
+    public class EndpointApiTest
+    {
+        [Fact]
+        public void can_register_new_project(string project)
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
